4961ebb934b14e64cedf964c5a8a979ed32c5526
Shailesh Prajapati <shailesh.welcome@gmail.com>
2018-07-10 12:38:42 +0530

    Atm
    State Bank of India is planning to introduce Automated Teller Machine (ATM) system
    for its customers to provide convenient withdrawal of Cash. A software is to be designed
    that will control ATM dispenser for cash (in multiples of Rs.500, 100, 50, 10 and 5 only)
    Write a program which will take Amount as input and gives output as break up of how
    many notes of each denomination will be dispensed.
    If not able to dispense amount, it should show error as &#39;Invalid Amount&#39;
    For e.g.:
    555 as input will return the output as:
    500 * 1
    50 * 1
    5 * 1

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rupee = int.Parse(Console.ReadLine());
+            if(rupee % 10 == 0 || rupee % 10 == 5)
+            {
+                CalculateDenominations(rupee);
+            }
+        }
+
+        public static void CalculateDenominations(int rupee)
+        {
+            int count = 0;
+            if (rupee >= 500)
+            {
+                count = rupee / 500;
+                Console.WriteLine($"500 * {count}");
+                CalculateDenominations(rupee % 500);
+            }
+            else if (rupee >= 100 && rupee < 500)
+            {
+                count = rupee / 100;
+                Console.WriteLine($"100 * {count}");
+                CalculateDenominations(rupee % 100);
+            }
+            else if (rupee >= 50 && rupee < 100)
+            {
+                count = rupee / 50;
+                Console.WriteLine($"50 * {count}");
+                CalculateDenominations(rupee % 50);
+            }
+            else if (rupee >= 10 && rupee < 50)
+            {
+                count = rupee / 10;
+                Console.WriteLine($"10 * {count}");
+                CalculateDenominations(rupee % 10);
+            }
+            else if (rupee >= 5 && rupee < 10)
+            {
+                count = rupee / 5;
+                Console.WriteLine($"5 * {count}");
+                CalculateDenominations(rupee % 5);
+            }
+            
+        }
+    }
+}
